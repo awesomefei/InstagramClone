837d6b2728ec64fbdd619b96293bc29e4f97f17c
acer <yibo001@gmail.com>
2018-06-22 11:12:43 -0700

    play with ParseUser

--- Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -24,6 +25,9 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,10 +35,63 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-/*
-//how to create an object
+
+
+
+
+
+    //check login or not
+    //ParseUser.logOut();
+     /* if(ParseUser.getCurrentUser()!= null){
+      Log.i("currentUser", "User logged in " + ParseUser.getCurrentUser().getUsername());
+    }else {
+      Log.i("currentUser", "User nor logged in");
+    }
+
+
+
+    //check the user's name and password
+    ParseUser.logInInBackground("reobpercival", "123", new LogInCallback() {
+      @Override
+      public void done(ParseUser user, ParseException e) {
+       if(user != null){
+         Log.i("Login", "Successfull");
+       }else{
+         Log.i("Login", "Successfull" + e.toString());
+       }
+      }
+    });
+
+
+
+//set a new user
+    ParseUser user = new ParseUser();
+    user.setUsername("reobpercival");
+    user.setPassword("myPass");
+
+    user.signUpInBackground(new SignUpCallback() {
+      @Override
+      public void done(ParseException e) {
+       if(e == null){
+         Log.i("sign up ", "Success");
+       }else{
+         Log.i("sign up ", "Failed");
+       }
+      }
+    });
+
+
+
+
+
+//how to create an class
+//Score is the name of the class
     ParseObject score = new ParseObject("Score");
     score.put("username", "rob");
     score.put("score", 86);
@@ -49,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         }
       }
     });
-*/
+
 //read data
     ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
     query.getInBackground("C9Bhdbw0Z6", new GetCallback<ParseObject>() {
@@ -61,6 +118,54 @@ public class MainActivity extends AppCompatActivity {
         }
       }
     });
+
+//update
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+    query.getInBackground("C9Bhdbw0Z6", new GetCallback<ParseObject>() {
+      @Override
+      public void done(ParseObject object, ParseException e) {
+        if(e == null && object != null){
+          object.put("score", 200);
+          object.saveInBackground();
+          Log.i("ObjectValue", object.getString("username"));
+          Log.i("ObjectValue", Integer.toString(object.getInt("score")));
+        }
+      }
+    });
+
+//find the exact data
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+    query.whereEqualTo("username", "Tommy");
+    query.setLimit(1);
+
+    query.findInBackground(new FindCallback<ParseObject>() {
+      @Override
+      public void done(List<ParseObject> objects, ParseException e) {
+        if(e == null){
+          Log.i("findInBackground", "retrieved " + objects.size() + " objects");
+          if(objects.size() > 0){
+            for(ParseObject object : objects){
+              Log.i("findInBackground", object.getString("username"));
+            }
+          }
+        }
+      }
+    });
+
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+    query.whereGreaterThan("score", 200);
+    query.findInBackground(new FindCallback<ParseObject>() {
+      @Override
+      public void done(List<ParseObject> objects, ParseException e) {
+        if(e == null && objects != null){
+          for(ParseObject object : objects){
+            object.put("score", object.getInt("score") + 50);
+            object.saveInBackground();
+          }
+        }
+      }
+    });
+*/
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
